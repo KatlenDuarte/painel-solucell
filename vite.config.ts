ee9a58f99c67f08b6d7e428050ce3b79b6b0504d
Katlen Duarte <104398149+KatlenDuarte@users.noreply.github.com>
2025-11-21 12:25:31 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-<<<<<<< HEAD
-  base: '/painel-solucell/',  // <== adiciona essa linha
-=======
   base: '/painel-solucell/',
->>>>>>> 7fbdc77bdc29a2ac42cfcf2275e16aaf2ead38f5
   plugins: [react()],
 })
